d522e4e54a4589c87227c6b99596761f6f91e8ee
Ioannis Tsampoulatidis <tsampoulatidis@gmail.com>
2012-10-20 01:05:34 +0300

    change editor and title length

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS `#__virtualcitytour360`;
 
 CREATE TABLE IF NOT EXISTS `#__virtualcitytour360` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `title` varchar(50) NOT NULL,
+  `title` varchar(255) NOT NULL,
   `catid` int(11) NOT NULL default '0',
-  `latitude` varchar(20) NOT NULL,
-  `longitude` varchar(20) NOT NULL,
+  `latitude` varchar(40) NOT NULL,
+  `longitude` varchar(40) NOT NULL,
   `description` text,
   `photos` text,
   `panoramas` text,
